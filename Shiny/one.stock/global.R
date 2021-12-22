f467891d2be6f16ce92268ad3b85f6dc6ef90b0a
QrDS <78227314+QrDS@users.noreply.github.com>
2021-12-22 18:15:12 +0100

    Update global.R

--- Shiny/one.stock/global.R
+++ Shiny/one.stock/global.R
@@ -14,12 +14,12 @@ library(quantmod)
 
 devtools::install_github('systematicinvestor/SIT.date')
 
-library(curl)
-curl_download('https://github.com/systematicinvestor/SIT/raw/master/SIT.tar.gz', 'sit',mode = 'wb',quiet=T)
-install.packages('sit', repos = NULL, type='source')
+#library(curl)
+#curl_download('https://github.com/systematicinvestor/SIT/raw/master/SIT.tar.gz', 'sit',mode = 'wb',quiet=T)
+devtools::install_github('systematicinvestor/SIT/raw/master/SIT.tar.gz')
+
 
 library(SIT)
-
 
 
 load.packages('Quandl')
