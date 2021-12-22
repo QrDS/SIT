e435bade5a7752b0b1032f6e14a5842d36e3b873
QrDS <78227314+QrDS@users.noreply.github.com>
2021-12-22 17:35:45 +0100

    Update global.R

--- Shiny/one.stock/global.R
+++ Shiny/one.stock/global.R
@@ -1,25 +1,24 @@
+
 library(shiny)
 library(xtable)
+
 
 ###############################################################################
 # Load Systematic Investor Toolbox (SIT)
 # http://systematicinvestor.wordpress.com/systematic-investor-toolbox/
 ###############################################################################
-setInternet2(TRUE)
-con = gzcon(url('https://github.com/systematicinvestor/SIT/raw/master/sit.gz', 'rb'))
-    source(con)
+if(!file.exists('../sit'))
+	shiny:::download('https://github.com/systematicinvestor/SIT/raw/master/sit.lite.gz', '../sit', mode = 'wb', quiet = TRUE)
+con = gzcon(file('../sit', 'rb'))
+	source(con)
 close(con)
- 
 
-load.packages('Quandl')
-load.packages('kernlab')
 
-load.packages('googleVis')
+
+
+
+library(SIT)
 
 load.packages('quantmod')
 if (!require(quantmod)) {
-  stop("This app requires the quantmod package. To install it, run 'install.packages(\"quantmod\")'.\n")
-}
-
-library(SIT)
 
